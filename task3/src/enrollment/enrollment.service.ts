258eebe2c3022b2867f66ac677fa3ddeb512caad
Fuad Hasan <fuad.cs22@gmail.com>
2026-04-06 19:23:28 +0600

    implement task3

---
+++ task3/src/enrollment/enrollment.service.ts
@@ -0,0 +1,34 @@
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import { CourseService } from '../course/course.service';
+import { NotificationService } from '../notification/notification.service';
+
+@Injectable()
+export class EnrollmentService {
+	constructor(
+		private readonly courseService: CourseService,
+		@Inject(forwardRef(() => NotificationService))
+		private readonly notificationService: NotificationService,
+	) {}
+
+	getEnrollments() {
+		return {
+			message: 'All enrollments fetched',
+			data: [],
+		};
+	}
+
+	enrollStudent(studentName: string, courseId: string) {
+		const course = this.courseService.getCourseById(courseId);
+		const notification = this.notificationService.sendNotification(
+			studentName,
+			`Enrollment successful for course ${courseId}`,
+		);
+
+		return {
+			message: 'Student enrolled successfully',
+			student: studentName,
+			course,
+			notification,
+		};
+	}
+}
